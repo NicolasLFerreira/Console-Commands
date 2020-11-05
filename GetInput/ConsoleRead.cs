4c5cac5c0dfc5e73264085c6b665e559d34ebcca
NicolasLFerreira <54772785+NicolasLFerreira@users.noreply.github.com>
2020-11-06 12:30:23 +1300

    Working command input split

---
+++ GetInput/ConsoleRead.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace SimpleConsole.GetInput
+{
+    class ConsoleRead
+    {
+        public static string[] CommandInput()
+        {
+            string input = Console.ReadLine();
+            return input.Split(' ');
+        }
+    }
+}
