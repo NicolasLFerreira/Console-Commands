29da400a30df600fc80cd47a4ddd5e72192300eb
NicolasLFerreira <54772785+NicolasLFerreira@users.noreply.github.com>
2020-11-06 12:31:55 +1300

    First commit

---
+++ Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace SimpleConsole
+{
+    using Terminal;
+    using Control;
+    using GetInput;
+    class Program
+    {
+        static void Main()
+        {
+            while (true)
+            {
+                string[] command = ConsoleRead.CommandInput();
+                object value = CommandReader.FunctionCall(command[0], command);
+                if (value != null) Console.WriteLine($"Got -> {value}");
+            }
+        }
+    }
+}
