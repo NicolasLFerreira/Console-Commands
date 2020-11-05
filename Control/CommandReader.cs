be6ba6ff35262a39947e1701b11fe89a1f331a21
NicolasLFerreira <54772785+NicolasLFerreira@users.noreply.github.com>
2020-11-06 12:31:39 +1300

    Working calls for oper, clear and invalid commands

---
+++ Control/CommandReader.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace SimpleConsole.Control
+{
+    using Terminal;
+    class CommandReader
+    {
+        public static object FunctionCall(string keyword, string[] command)
+        {
+            switch (keyword)
+            {
+                case "clear":
+                    Commands.Clear();
+                    return null;
+                case "oper":
+                    return Commands.Oper(command);
+                default:
+                    return "There's no such command";
+            }
+        }
+    }
+}
